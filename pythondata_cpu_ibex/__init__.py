cda149bf77382688fad9a3749adfe627867ebdd1
LiteX Robot <robot@timvideos.us>
2021-02-15 18:14:44 +0000

    Updating pythondata-cpu-ibex to 0.0.post2058
    Updated data to v0.0-1968-g7cee76bf based on 7cee76bf05134c683154b26f4d18c2843cbfca64 from https://github.com/lowRISC/ibex.
    > commit 7cee76bf05134c683154b26f4d18c2843cbfca64
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed Feb 10 16:36:26 2021 +0000
    >
    >     [dv] Reorder checks in sim.py
    >
    >     The UVM log should be checked for failures before attempting to process
    >     the core trace log. A simulation failure could mean the trace log
    >     doesn't exist and is is preferable to report the simulation error from
    >     the log rather than trace not found as a failure cause.
    >
    
    Updated using 0.0.post90 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,60 +4,35 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2057"
-version_tuple = (0, 0, 2057)
+version_str = "0.0.post2058"
+version_tuple = (0, 0, 2058)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2057")
+    pversion = V("0.0.post2058")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1967"
-data_version_tuple = (0, 0, 1967)
+data_version_str = "0.0.post1968"
+data_version_tuple = (0, 0, 1968)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1967")
+    pdata_version = V("0.0.post1968")
 except ImportError:
     pass
-data_git_hash = "0cb2afffa9893444dc2979bb290fa2c08ac384d7"
-data_git_describe = "v0.0-1967-g0cb2afff"
+data_git_hash = "7cee76bf05134c683154b26f4d18c2843cbfca64"
+data_git_describe = "v0.0-1968-g7cee76bf"
 data_git_msg = """\
-commit 0cb2afffa9893444dc2979bb290fa2c08ac384d7
+commit 7cee76bf05134c683154b26f4d18c2843cbfca64
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Mon Feb 1 18:40:28 2021 +0000
+Date:   Wed Feb 10 16:36:26 2021 +0000
 
-    Update google_riscv-dv to google/riscv-dv@0b62525
+    [dv] Reorder checks in sim.py
     
-    Update code from upstream repository https://github.com/google/riscv-
-    dv to revision 0b625258549e733082c12e5dc749f05aefb07d5a
-    
-    * Add a knob to use rounding mode from the instruction (google/riscv-
-      dv#767) (taoliug)
-    * Add rounding mode support for floating point arithmetic instructions
-      (google/riscv-dv#766) (taoliug)
-    * Fix syntax issue (google/riscv-dv#765) (taoliug)
-    * Add riscv_amo_instr (aneels3)
-    * convert string to enum type (ishita71)
-    * Remove unintended errors in the coverage flow (google/riscv-dv#757)
-      (taoliug)
-    * Fix c_test handling in the YAML testlist (google/riscv-dv#756)
-      (taoliug)
-    * Add support for new Spike trace format (google/riscv-dv#755) (Daniel
-      Bates)
-    * Fix google/riscv-dv#751 for floating point coverage (Weicai Yang)
-    * Fix issues with implemented TODO's (aneels3)
-    * fix randomize_gpr (aneels3)
-    * Add file riscv_b_instr.py (ishita71)
-    * add std_randomize todo (pvipsyash)
-    * Add todo for floating_point test (ShraddhaDevaiya)
-    * Add scripts to integrate with Metrics regression platform (Aimee
-      Sutton)
-    
-    Includes a fix to dv/uvm/core_ibex/sim.py to use `asm_test` rather than
-    `asm_tests` due to changes in RISCV-DV
-    
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    The UVM log should be checked for failures before attempting to process
+    the core trace log. A simulation failure could mean the trace log
+    doesn't exist and is is preferable to report the simulation error from
+    the log rather than trace not found as a failure cause.
 
 """
 
