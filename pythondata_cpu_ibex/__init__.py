9ff8a58c526c1f1f0faff2442e829edfe45ec7ea
LiteX Robot <robot@timvideos.us>
2021-02-25 16:26:51 +0000

    Updating pythondata-cpu-ibex to 0.0.post2062
    Updated data to v0.0-1971-g6ebc6bcb based on 6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69 from https://github.com/lowRISC/ibex.
    > commit 6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Feb 24 15:07:06 2021 +0000
    >
    >     [simple_system] Fix type for mhpmcounter_get
    >
    >     It's probably clearer if this 64-bit counter is treated as a uint64_t,
    >     not an int64_t (the code using it downstream expects non-negative
    >     values).
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,41 +4,34 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2061"
-version_tuple = (0, 0, 2061)
+version_str = "0.0.post2062"
+version_tuple = (0, 0, 2062)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2061")
+    pversion = V("0.0.post2062")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1970"
-data_version_tuple = (0, 0, 1970)
+data_version_str = "0.0.post1971"
+data_version_tuple = (0, 0, 1971)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1970")
+    pdata_version = V("0.0.post1971")
 except ImportError:
     pass
-data_git_hash = "99b8f612234adc76e70cfc53f00a451b3ad4662a"
-data_git_describe = "v0.0-1970-g99b8f612"
+data_git_hash = "6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69"
+data_git_describe = "v0.0-1971-g6ebc6bcb"
 data_git_msg = """\
-commit 99b8f612234adc76e70cfc53f00a451b3ad4662a
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Wed Feb 10 16:25:51 2021 +0000
+commit 6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Feb 24 15:07:06 2021 +0000
 
-    [rtl] Debug mode controller changes
+    [simple_system] Fix type for mhpmcounter_get
     
-    * `if` in `DBG_TAKEN_IF` is needless as the conditions it checks will be
-      true if controller enters `DBG_TAKEN_IF` state
-    
-    * flop `enter_debug_mode` so `FLUSH` state looks at what
-      `enter_debug_mode` was when it was seen in `DECODE` state rather than
-      what it has become. In particular the controller could enter `FLUSH`
-      on the basis of performing a WFI then divert down the debug control
-      path due to a new debug request being raised. In this instance it is
-      preferable for the WFI to complete entering `SLEEP` before the debug
-      request wakes the core back up.
+    It's probably clearer if this 64-bit counter is treated as a uint64_t,
+    not an int64_t (the code using it downstream expects non-negative
+    values).
 
 """
 
