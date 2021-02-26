1bf8bc3424a5de9d38e8c464e975475a3ed60170
LiteX Robot <robot@timvideos.us>
2021-02-26 18:14:22 +0000

    Updating pythondata-cpu-ibex to 0.0.post2063
    Updated data to v0.0-1972-gc3bd4fa7 based on c3bd4fa7ef5143045f8dc15d56b342a62f83cb86 from https://github.com/lowRISC/ibex.
    > commit c3bd4fa7ef5143045f8dc15d56b342a62f83cb86
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Tue Jan 26 16:37:20 2021 +0000
    >
    >     [rtl] Remove paths between dmem and imem signals
    >
    >     Prior to this change Ibex had multiple feedthrough paths from the data
    >     memory interface to the instruction memory interface. This existed
    >     because Ibex would hold off doing a instruction fetch for a jump or
    >     branch if there was a outstanding memory request. It would wait for the
    >     response to be available so either the jump or branch would occur or an
    >     exception was taken.
    >
    >     With this change the branch or jump will speculatively begin the
    >     instruction fetch whilst there is an outstanding memory request. Should
    >     an exception result from the memory request the fetch will be discarded
    >     and the exception taken as normal.
    >
    >     An alternative fix would not factor the data error response
    >     (data_err_i) directly into the controller logic for branches and jumps.
    >     With this option new stall cycles would be introduced anywhere a branch
    >     or jump immediately follows a memory instruction which would have an
    >     adverse impact on performance.
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,34 +4,48 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2062"
-version_tuple = (0, 0, 2062)
+version_str = "0.0.post2063"
+version_tuple = (0, 0, 2063)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2062")
+    pversion = V("0.0.post2063")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1971"
-data_version_tuple = (0, 0, 1971)
+data_version_str = "0.0.post1972"
+data_version_tuple = (0, 0, 1972)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1971")
+    pdata_version = V("0.0.post1972")
 except ImportError:
     pass
-data_git_hash = "6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69"
-data_git_describe = "v0.0-1971-g6ebc6bcb"
+data_git_hash = "c3bd4fa7ef5143045f8dc15d56b342a62f83cb86"
+data_git_describe = "v0.0-1972-gc3bd4fa7"
 data_git_msg = """\
-commit 6ebc6bcb9f4574ab2adff9ec1c27f052ee620b69
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Feb 24 15:07:06 2021 +0000
+commit c3bd4fa7ef5143045f8dc15d56b342a62f83cb86
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Tue Jan 26 16:37:20 2021 +0000
 
-    [simple_system] Fix type for mhpmcounter_get
+    [rtl] Remove paths between dmem and imem signals
     
-    It's probably clearer if this 64-bit counter is treated as a uint64_t,
-    not an int64_t (the code using it downstream expects non-negative
-    values).
+    Prior to this change Ibex had multiple feedthrough paths from the data
+    memory interface to the instruction memory interface. This existed
+    because Ibex would hold off doing a instruction fetch for a jump or
+    branch if there was a outstanding memory request. It would wait for the
+    response to be available so either the jump or branch would occur or an
+    exception was taken.
+    
+    With this change the branch or jump will speculatively begin the
+    instruction fetch whilst there is an outstanding memory request. Should
+    an exception result from the memory request the fetch will be discarded
+    and the exception taken as normal.
+    
+    An alternative fix would not factor the data error response
+    (data_err_i) directly into the controller logic for branches and jumps.
+    With this option new stall cycles would be introduced anywhere a branch
+    or jump immediately follows a memory instruction which would have an
+    adverse impact on performance.
 
 """
 
