4549d75da8bc6e410e7953336c752e8698488585
LiteX Robot <robot@timvideos.us>
2021-03-01 10:45:49 +0000

    Updating pythondata-cpu-ibex to 0.0.post2066
    Updated data to v0.0-1975-gfaa1e98a based on faa1e98a6e1cfa8bea95a86d1ba8db5f7818ac0d from https://github.com/lowRISC/ibex.
    > commit faa1e98a6e1cfa8bea95a86d1ba8db5f7818ac0d
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Feb 26 10:04:58 2021 +0000
    >
    >     [dv] Fix bug in sim.py and type in testlist
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,48 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2063"
-version_tuple = (0, 0, 2063)
+version_str = "0.0.post2066"
+version_tuple = (0, 0, 2066)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2063")
+    pversion = V("0.0.post2066")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1972"
-data_version_tuple = (0, 0, 1972)
+data_version_str = "0.0.post1975"
+data_version_tuple = (0, 0, 1975)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1972")
+    pdata_version = V("0.0.post1975")
 except ImportError:
     pass
-data_git_hash = "c3bd4fa7ef5143045f8dc15d56b342a62f83cb86"
-data_git_describe = "v0.0-1972-gc3bd4fa7"
+data_git_hash = "faa1e98a6e1cfa8bea95a86d1ba8db5f7818ac0d"
+data_git_describe = "v0.0-1975-gfaa1e98a"
 data_git_msg = """\
-commit c3bd4fa7ef5143045f8dc15d56b342a62f83cb86
+commit faa1e98a6e1cfa8bea95a86d1ba8db5f7818ac0d
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Jan 26 16:37:20 2021 +0000
+Date:   Fri Feb 26 10:04:58 2021 +0000
 
-    [rtl] Remove paths between dmem and imem signals
-    
-    Prior to this change Ibex had multiple feedthrough paths from the data
-    memory interface to the instruction memory interface. This existed
-    because Ibex would hold off doing a instruction fetch for a jump or
-    branch if there was a outstanding memory request. It would wait for the
-    response to be available so either the jump or branch would occur or an
-    exception was taken.
-    
-    With this change the branch or jump will speculatively begin the
-    instruction fetch whilst there is an outstanding memory request. Should
-    an exception result from the memory request the fetch will be discarded
-    and the exception taken as normal.
-    
-    An alternative fix would not factor the data error response
-    (data_err_i) directly into the controller logic for branches and jumps.
-    With this option new stall cycles would be introduced anywhere a branch
-    or jump immediately follows a memory instruction which would have an
-    adverse impact on performance.
+    [dv] Fix bug in sim.py and type in testlist
 
 """
 
