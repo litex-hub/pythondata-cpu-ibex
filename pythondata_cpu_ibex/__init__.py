8c6282ae5b00c405505f3e02e0794fab86184bf2
LiteX Robot <robot@timvideos.us>
2021-03-04 13:16:23 +0000

    Updating pythondata-cpu-ibex to 0.0.post2072
    Updated data to v0.0-1981-g6d9e1aca based on 6d9e1aca8ad7cf09d4effcde97e471d6c213ead1 from https://github.com/lowRISC/ibex.
    > commit 6d9e1aca8ad7cf09d4effcde97e471d6c213ead1
    > Author: Tom Roberts <tomroberts@lowrisc.org>
    > Date:   Thu Mar 4 11:42:59 2021 +0000
    >
    >     [rtl] Minor lint fix in ibex_core.sv
    >
    >     Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2071"
-version_tuple = (0, 0, 2071)
+version_str = "0.0.post2072"
+version_tuple = (0, 0, 2072)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2071")
+    pversion = V("0.0.post2072")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1980"
-data_version_tuple = (0, 0, 1980)
+data_version_str = "0.0.post1981"
+data_version_tuple = (0, 0, 1981)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1980")
+    pdata_version = V("0.0.post1981")
 except ImportError:
     pass
-data_git_hash = "53634991906dbf0efde1018b6a6fb46801d75277"
-data_git_describe = "v0.0-1980-g53634991"
+data_git_hash = "6d9e1aca8ad7cf09d4effcde97e471d6c213ead1"
+data_git_describe = "v0.0-1981-g6d9e1aca"
 data_git_msg = """\
-commit 53634991906dbf0efde1018b6a6fb46801d75277
-Author: Vladimir Rozic <vrozic@lowrisc.org>
-Date:   Wed Mar 3 14:10:39 2021 +0000
+commit 6d9e1aca8ad7cf09d4effcde97e471d6c213ead1
+Author: Tom Roberts <tomroberts@lowrisc.org>
+Date:   Thu Mar 4 11:42:59 2021 +0000
 
-    [rtl] Add MCOUNTEREN CSR
+    [rtl] Minor lint fix in ibex_core.sv
     
-    This commit adds the MCOUNTEREN CSR as required by the RISC-V spec.
-    The register is defined as WARL. At the moment, Ibex doesn't enable U-mode
-    access to the performance montiors. Consequently, writes to the register are
-    ignored and it reads as zero which is okay according to the spec.
-    
-    This resolves lowRISC/Ibex#1278 .
+    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
 
 """
 
