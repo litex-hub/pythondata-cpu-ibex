77280c24c2026ad5f856bf0bde688996d55e9f78
LiteX Robot <robot@timvideos.us>
2021-03-14 20:14:17 +0000

    Updating pythondata-cpu-ibex to 0.0.post2081
    Updated data to v0.0-1986-g6576247a based on 6576247a1ee68baf4d1abd43e04a5bfdde169ecb from https://github.com/lowRISC/ibex.
    > commit 6576247a1ee68baf4d1abd43e04a5bfdde169ecb
    > Author: Udi Jonnalagadda <udij@google.com>
    > Date:   Fri Mar 12 17:55:39 2021 -0800
    >
    >     [ci/ibex] temporarily remove pmp_full_random_test
    >
    >     this test is arbitrarily failing in regressions on a Spike timeout,
    >     temporarily remove this to avoid blocking.
    >
    >     @udinator to fix this in the near future.
    >
    >     Signed-off-by: Udi Jonnalagadda <udij@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,36 +4,37 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2080"
-version_tuple = (0, 0, 2080)
+version_str = "0.0.post2081"
+version_tuple = (0, 0, 2081)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2080")
+    pversion = V("0.0.post2081")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post1985"
-data_version_tuple = (0, 0, 1985)
+data_version_str = "0.0.post1986"
+data_version_tuple = (0, 0, 1986)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post1985")
+    pdata_version = V("0.0.post1986")
 except ImportError:
     pass
-data_git_hash = "42827fc9cd0b2043d5d179cae46b0238a55d3652"
-data_git_describe = "v0.0-1985-g42827fc9"
+data_git_hash = "6576247a1ee68baf4d1abd43e04a5bfdde169ecb"
+data_git_describe = "v0.0-1986-g6576247a"
 data_git_msg = """\
-commit 42827fc9cd0b2043d5d179cae46b0238a55d3652
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Wed Mar 10 14:07:28 2021 +0000
+commit 6576247a1ee68baf4d1abd43e04a5bfdde169ecb
+Author: Udi Jonnalagadda <udij@google.com>
+Date:   Fri Mar 12 17:55:39 2021 -0800
 
-    [rtl/icache] Switch ECC granularity to 32bits
+    [ci/ibex] temporarily remove pmp_full_random_test
     
-    Changes the ECC granularity in the data RAMs from 64bit to 32bit. This
-    is to align with an upcoming change in bus ECC. Relates to
-    lowRISC/opentitan#5450
+    this test is arbitrarily failing in regressions on a Spike timeout,
+    temporarily remove this to avoid blocking.
     
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    @udinator to fix this in the near future.
+    
+    Signed-off-by: Udi Jonnalagadda <udij@google.com>
 
 """
 
