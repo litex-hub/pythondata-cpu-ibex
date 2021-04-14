8bc9adf6f4f4b33d259bf6eff9f6cb47d035fe94
LiteX Robot <robot@timvideos.us>
2021-04-14 11:04:49 +0000

    Updating pythondata-cpu-ibex to 0.0.post2109
    Updated data to v0.0-2014-g48a886a2 based on 48a886a25fc788ea4529872047811f79bca4877f from https://github.com/lowRISC/ibex.
    > commit 48a886a25fc788ea4529872047811f79bca4877f
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Tue Feb 9 15:35:59 2021 +0000
    >
    >     Update README to match design
    >
    >     Since this part of the README was written the design moved on. Let's
    >     update it. This update follows the text we have at
    >     https://ibex-core.readthedocs.io/en/latest/index.html.
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,37 +4,34 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2107"
-version_tuple = (0, 0, 2107)
+version_str = "0.0.post2109"
+version_tuple = (0, 0, 2109)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2107")
+    pversion = V("0.0.post2109")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2012"
-data_version_tuple = (0, 0, 2012)
+data_version_str = "0.0.post2014"
+data_version_tuple = (0, 0, 2014)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2012")
+    pdata_version = V("0.0.post2014")
 except ImportError:
     pass
-data_git_hash = "69e715b2878c1ee7fdca2c3d1ed3396e6bdadb64"
-data_git_describe = "v0.0-2012-g69e715b2"
+data_git_hash = "48a886a25fc788ea4529872047811f79bca4877f"
+data_git_describe = "v0.0-2014-g48a886a2"
 data_git_msg = """\
-commit 69e715b2878c1ee7fdca2c3d1ed3396e6bdadb64
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Mon Apr 12 11:57:18 2021 +0100
+commit 48a886a25fc788ea4529872047811f79bca4877f
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Tue Feb 9 15:35:59 2021 +0000
 
-    [dv] Improvements to functional coverage
+    Update README to match design
     
-    This adds more instruction categories and corrects various issues in the
-    categorization code. Further cross coverage has been added including
-    illegal bins to remove bins that cannot occur.
-    
-    The concept of using SVAs with cross coverage has been dropped. The
-    systemverilog scheduling model makes the concept unworkable.
+    Since this part of the README was written the design moved on. Let's
+    update it. This update follows the text we have at
+    https://ibex-core.readthedocs.io/en/latest/index.html.
 
 """
 
