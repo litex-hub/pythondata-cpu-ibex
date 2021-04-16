57c80a918560cac3099cdf8057ddcebcddf9f114
LiteX Robot <robot@timvideos.us>
2021-04-16 09:04:48 +0000

    Updating pythondata-cpu-ibex to 0.0.post2111
    Updated data to v0.0-2016-g0e7117fb based on 0e7117fbec434fcfdf2bcce4ed379d34cdbe68b4 from https://github.com/lowRISC/ibex.
    > commit 0e7117fbec434fcfdf2bcce4ed379d34cdbe68b4
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Thu Apr 15 18:23:38 2021 -0700
    >
    >     [lockstep] Introduce optimization barrier around lockstep Ibex
    >
    >     Certain synthesis tools like DC are very smart at optimizing away redundant logic.
    >     Hence, we have to insert an optimization barrier at the IOs of the lockstep Ibex.
    >     This is achieved by manually buffering each bit using prim_buf.
    >     Our Xilinx and DC synthesis flows make sure that these buffers cannot be optimized
    >     away using keep attributes (Vivado) and size_only constraints (DC).
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,37 +4,38 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2110"
-version_tuple = (0, 0, 2110)
+version_str = "0.0.post2111"
+version_tuple = (0, 0, 2111)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2110")
+    pversion = V("0.0.post2111")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2015"
-data_version_tuple = (0, 0, 2015)
+data_version_str = "0.0.post2016"
+data_version_tuple = (0, 0, 2016)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2015")
+    pdata_version = V("0.0.post2016")
 except ImportError:
     pass
-data_git_hash = "1b59c67b5064d03ec62a2877de4e94d2802a2225"
-data_git_describe = "v0.0-2015-g1b59c67b"
+data_git_hash = "0e7117fbec434fcfdf2bcce4ed379d34cdbe68b4"
+data_git_describe = "v0.0-2016-g0e7117fb"
 data_git_msg = """\
-commit 1b59c67b5064d03ec62a2877de4e94d2802a2225
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Apr 13 14:34:05 2021 +0100
+commit 0e7117fbec434fcfdf2bcce4ed379d34cdbe68b4
+Author: Michael Schaffner <msf@google.com>
+Date:   Thu Apr 15 18:23:38 2021 -0700
 
-    [dv] Remove MISA from csr_description.yaml
+    [lockstep] Introduce optimization barrier around lockstep Ibex
     
-    The value of `misa` will change depending on whether M or B are enabled.
-    The presence and read values of other CSRs may also depend upon the Ibex
-    configuration. A fix is required to allow riscv_csr_test to deal with
-    different CSR descriptions for different Ibex configurations. For now
-    just comment out `misa` from the descriptions file to enable
-    riscv_csr_test to run on a wider range of configurations.
+    Certain synthesis tools like DC are very smart at optimizing away redundant logic.
+    Hence, we have to insert an optimization barrier at the IOs of the lockstep Ibex.
+    This is achieved by manually buffering each bit using prim_buf.
+    Our Xilinx and DC synthesis flows make sure that these buffers cannot be optimized
+    away using keep attributes (Vivado) and size_only constraints (DC).
+    
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
