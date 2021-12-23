e1f9ecbd74fda6ad32299cb989378d573fa4a16f
LiteX Robot <robot@timvideos.us>
2021-12-23 22:03:48 +0000

    Updating pythondata-cpu-ibex to 0.0.post2245
    Updated data to v0.0-2132-g410ffd34 based on 410ffd349da1d4c13f77b208bb3b93adb154fac0 from https://github.com/lowRISC/ibex.
    > commit 410ffd349da1d4c13f77b208bb3b93adb154fac0
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Fri Dec 10 22:16:07 2021 +0100
    >
    >     [bitmanip, doc] Update info on bitmanip support and area numbers
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post113 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,43 +4,41 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2226"
-version_tuple = (0, 0, 2226)
+version_str = "0.0.post2245"
+version_tuple = (0, 0, 2245)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2226")
+    pversion = V("0.0.post2245")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2114"
-data_version_tuple = (0, 0, 2114)
+data_version_str = "0.0.post2132"
+data_version_tuple = (0, 0, 2132)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2114")
+    pdata_version = V("0.0.post2132")
 except ImportError:
     pass
-data_git_hash = "40dab87448db052b40758075289382fda7b41fc1"
-data_git_describe = "v0.0-2114-g40dab874"
+data_git_hash = "410ffd349da1d4c13f77b208bb3b93adb154fac0"
+data_git_describe = "v0.0-2132-g410ffd34"
 data_git_msg = """\
-commit 40dab87448db052b40758075289382fda7b41fc1
+commit 410ffd349da1d4c13f77b208bb3b93adb154fac0
 Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Nov 26 16:40:33 2021 +0100
+Date:   Fri Dec 10 22:16:07 2021 +0100
 
-    [rtl, bitmanip] Clarify situation around zext.[bh] pseudo-instructions
-    
-    This is related to lowRISC/Ibex#1228.
+    [bitmanip, doc] Update info on bitmanip support and area numbers
     
     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post112"
-tool_version_tuple = (0, 0, 112)
+tool_version_str = "0.0.post113"
+tool_version_tuple = (0, 0, 113)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post112")
+    ptool_version = V("0.0.post113")
 except ImportError:
     pass
 
