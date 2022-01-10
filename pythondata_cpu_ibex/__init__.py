81d82472e854f800e5f8082c1a26ecbf93ddd711
LiteX Robot <robot@timvideos.us>
2022-01-10 18:01:42 +0000

    Updating pythondata-cpu-ibex to 0.0.post2252
    Updated data to v0.0-2134-g8c014886 based on 8c01488624b42066fcb268dcecf20b008146dbbd from https://github.com/lowRISC/ibex.
    > commit 8c01488624b42066fcb268dcecf20b008146dbbd
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Fri Jan 7 14:15:41 2022 +0100
    >
    >     [rtl] Document lockstep reset generation mechanism
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post118 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2251"
-version_tuple = (0, 0, 2251)
+version_str = "0.0.post2252"
+version_tuple = (0, 0, 2252)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2251")
+    pversion = V("0.0.post2252")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Fri Jan 7 14:15:41 2022 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post117"
-tool_version_tuple = (0, 0, 117)
+tool_version_str = "0.0.post118"
+tool_version_tuple = (0, 0, 118)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post117")
+    ptool_version = V("0.0.post118")
 except ImportError:
     pass
 
