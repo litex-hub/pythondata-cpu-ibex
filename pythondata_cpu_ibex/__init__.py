5b3d6d1cab929b54de77807dbd6e8022beb2d043
LiteX Robot <robot@timvideos.us>
2022-01-25 15:27:41 +0000

    Updating pythondata-cpu-ibex to 0.0.post2266
    Updated data to v0.0-2144-gc0f67946 based on c0f67946f21838de36088b6a25573051b925207e from https://github.com/lowRISC/ibex.
    > commit c0f67946f21838de36088b6a25573051b925207e
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Mon Jan 24 18:46:46 2022 +0000
    >
    >     [rtl,doc] Add double fault detection.
    >
    >     Fixes #1117
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2265"
-version_tuple = (0, 0, 2265)
+version_str = "0.0.post2266"
+version_tuple = (0, 0, 2266)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2265")
+    pversion = V("0.0.post2266")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2143"
-data_version_tuple = (0, 0, 2143)
+data_version_str = "0.0.post2144"
+data_version_tuple = (0, 0, 2144)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2143")
+    pdata_version = V("0.0.post2144")
 except ImportError:
     pass
-data_git_hash = "97fa5cf28024b882ffa1acbe81f6b6c865715840"
-data_git_describe = "v0.0-2143-g97fa5cf2"
+data_git_hash = "c0f67946f21838de36088b6a25573051b925207e"
+data_git_describe = "v0.0-2144-gc0f67946"
 data_git_msg = """\
-commit 97fa5cf28024b882ffa1acbe81f6b6c865715840
+commit c0f67946f21838de36088b6a25573051b925207e
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Jan 21 17:10:10 2022 +0000
+Date:   Mon Jan 24 18:46:46 2022 +0000
 
-    [rtl,doc] Add customisable PMP reset values
+    [rtl,doc] Add double fault detection.
     
-    Fixes #1423
+    Fixes #1117
 
 """
 
