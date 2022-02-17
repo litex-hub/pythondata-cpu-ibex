8802428dc03d0709fc8d1cd7b9305f79f1690290
LiteX Robot <robot@timvideos.us>
2022-02-17 15:29:37 +0000

    Updating pythondata-cpu-ibex to 0.0.post2282
    Updated data to v0.0-2156-g9943f9a4 based on 9943f9a42c36062f86f84cc1082e6995b8d328e4 from https://github.com/lowRISC/ibex.
    > commit 9943f9a42c36062f86f84cc1082e6995b8d328e4
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Thu Feb 17 13:50:08 2022 +0000
    >
    >     [rtl, doc] Seperate major alert into internal and bus
    >
    >     This is to allow more consistent signalling in systems that integrate
    >     Ibex (e.g. OpenTitan) so bus integrity errors external to Ibex and one's
    >     detected within Ibex can be fed into the same alert whilst seperating
    >     out Ibex's various internal alert causes.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,35 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2281"
-version_tuple = (0, 0, 2281)
+version_str = "0.0.post2282"
+version_tuple = (0, 0, 2282)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2281")
+    pversion = V("0.0.post2282")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2155"
-data_version_tuple = (0, 0, 2155)
+data_version_str = "0.0.post2156"
+data_version_tuple = (0, 0, 2156)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2155")
+    pdata_version = V("0.0.post2156")
 except ImportError:
     pass
-data_git_hash = "e84e7de53f326809a6bc11e1ba7cdb440d60719a"
-data_git_describe = "v0.0-2155-ge84e7de5"
+data_git_hash = "9943f9a42c36062f86f84cc1082e6995b8d328e4"
+data_git_describe = "v0.0-2156-g9943f9a4"
 data_git_msg = """\
-commit e84e7de53f326809a6bc11e1ba7cdb440d60719a
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Feb 16 12:57:47 2022 +0000
+commit 9943f9a42c36062f86f84cc1082e6995b8d328e4
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Thu Feb 17 13:50:08 2022 +0000
 
-    Fix narrowing conversion warning in cosim_dpi.cc
+    [rtl, doc] Seperate major alert into internal and bus
+    
+    This is to allow more consistent signalling in systems that integrate
+    Ibex (e.g. OpenTitan) so bus integrity errors external to Ibex and one's
+    detected within Ibex can be fed into the same alert whilst seperating
+    out Ibex's various internal alert causes.
 
 """
 
