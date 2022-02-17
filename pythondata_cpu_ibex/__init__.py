051389e8d888a2ccb5dbef65d45a6ed11d2609a9
LiteX Robot <robot@timvideos.us>
2022-02-17 16:38:04 +0000

    Updating pythondata-cpu-ibex to 0.0.post2285
    Updated data to v0.0-2159-g302bb651 based on 302bb6516166b81de80858c5b045680483797328 from https://github.com/lowRISC/ibex.
    > commit 302bb6516166b81de80858c5b045680483797328
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Feb 17 14:58:39 2022 +0000
    >
    >     [doc] Update bitmanip list in integration.rst
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,35 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2282"
-version_tuple = (0, 0, 2282)
+version_str = "0.0.post2285"
+version_tuple = (0, 0, 2285)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2282")
+    pversion = V("0.0.post2285")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2156"
-data_version_tuple = (0, 0, 2156)
+data_version_str = "0.0.post2159"
+data_version_tuple = (0, 0, 2159)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2156")
+    pdata_version = V("0.0.post2159")
 except ImportError:
     pass
-data_git_hash = "9943f9a42c36062f86f84cc1082e6995b8d328e4"
-data_git_describe = "v0.0-2156-g9943f9a4"
+data_git_hash = "302bb6516166b81de80858c5b045680483797328"
+data_git_describe = "v0.0-2159-g302bb651"
 data_git_msg = """\
-commit 9943f9a42c36062f86f84cc1082e6995b8d328e4
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Thu Feb 17 13:50:08 2022 +0000
+commit 302bb6516166b81de80858c5b045680483797328
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu Feb 17 14:58:39 2022 +0000
 
-    [rtl, doc] Seperate major alert into internal and bus
-    
-    This is to allow more consistent signalling in systems that integrate
-    Ibex (e.g. OpenTitan) so bus integrity errors external to Ibex and one's
-    detected within Ibex can be fed into the same alert whilst seperating
-    out Ibex's various internal alert causes.
+    [doc] Update bitmanip list in integration.rst
 
 """
 
