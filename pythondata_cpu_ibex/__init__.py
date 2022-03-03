2cd4e96415fac6c15c311709a8f237dec55e2363
LiteX Robot <robot@timvideos.us>
2022-03-03 14:28:16 +0000

    Updating pythondata-cpu-ibex to 0.0.post2292
    Updated data to v0.0-2166-gc15f3b88 based on c15f3b88883781808eaa96bda205a9bdaff5eb98 from https://github.com/lowRISC/ibex.
    > commit c15f3b88883781808eaa96bda205a9bdaff5eb98
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Mar 2 23:46:26 2022 +0000
    >
    >     [icache] Define some fake DPI functions to simplify linking
    >
    >     This is triggered by the fact that if the ICache parameter is false
    >     then we don't instantiate the ibex_icache module. For verilator
    >     simulations, the module is then discarded entirely, which means that
    >     its two DPI functions are not defined. That's unfortunate because
    >     we're also compiling the code in scrambled_ecc32_mem_area.cc, which
    >     expects the functions to be defined.
    >
    >     The obvious solution (don't include scrambled_ecc32_mem_area.cc if you
    >     don't have an icache) isn't easy to do, because FuseSoc doesn't
    >     currently allow us to use parameters to configure its dependency
    >     tree (see fusesoc issue 438 for a discussion).
    >
    >     The super-clever solution that I came up with before(!) was to declare
    >     these symbols as weak in the C++ code. That way, we can do a runtime
    >     check to make sure that no-one is silly enough to call them without an
    >     icache, but everything will still build properly either way.
    >
    >     Unfortunately, that doesn't work well with xcelium simulations.
    >     Xcelium turns out to compile all the C++ code into one .so library and
    >     generate functions for exported DPI functions in another. These two
    >     solibs then get loaded at runtime with dlopen(). But this doesn't work
    >     with weak symbols: in fact, it seems you end up with the C++ version
    >     every time. Boo!
    >
    >     So let's be stupider about it and define (bogus) versions of the DPI
    >     functions in this case. Fortunately, both of them are designed to
    >     return zero on failure so we can just return zero and needn't worry
    >     too much.
    >
    >     The idea is that when this lands, we can revert the OpenTitan change
    >     that switched the C++ code to using weak symbols and Xcelium
    >     simulations will start working.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,39 +4,63 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2291"
-version_tuple = (0, 0, 2291)
+version_str = "0.0.post2292"
+version_tuple = (0, 0, 2292)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2291")
+    pversion = V("0.0.post2292")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2165"
-data_version_tuple = (0, 0, 2165)
+data_version_str = "0.0.post2166"
+data_version_tuple = (0, 0, 2166)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2165")
+    pdata_version = V("0.0.post2166")
 except ImportError:
     pass
-data_git_hash = "bdf2f2b44076c8e9a00fa6dcbee0a409026e5c3a"
-data_git_describe = "v0.0-2165-gbdf2f2b4"
+data_git_hash = "c15f3b88883781808eaa96bda205a9bdaff5eb98"
+data_git_describe = "v0.0-2166-gc15f3b88"
 data_git_msg = """\
-commit bdf2f2b44076c8e9a00fa6dcbee0a409026e5c3a
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Wed Feb 23 16:09:57 2022 +0000
+commit c15f3b88883781808eaa96bda205a9bdaff5eb98
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Mar 2 23:46:26 2022 +0000
 
-    [ibex, dv] Added agent configuration for ibex_mem_intf_response_agent
+    [icache] Define some fake DPI functions to simplify linking
     
-    Defining agent configuration for any agent is a standard UVM flow and is
-    a cleaner flow for defining delay between driving sequence items,
-    passing virtual interface etc.
+    This is triggered by the fact that if the ICache parameter is false
+    then we don't instantiate the ibex_icache module. For verilator
+    simulations, the module is then discarded entirely, which means that
+    its two DPI functions are not defined. That's unfortunate because
+    we're also compiling the code in scrambled_ecc32_mem_area.cc, which
+    expects the functions to be defined.
     
-    Agent configuration has been added to the existing agent to make delay
-    configuration more flexible in the future.
+    The obvious solution (don't include scrambled_ecc32_mem_area.cc if you
+    don't have an icache) isn't easy to do, because FuseSoc doesn't
+    currently allow us to use parameters to configure its dependency
+    tree (see fusesoc issue 438 for a discussion).
     
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    The super-clever solution that I came up with before(!) was to declare
+    these symbols as weak in the C++ code. That way, we can do a runtime
+    check to make sure that no-one is silly enough to call them without an
+    icache, but everything will still build properly either way.
+    
+    Unfortunately, that doesn't work well with xcelium simulations.
+    Xcelium turns out to compile all the C++ code into one .so library and
+    generate functions for exported DPI functions in another. These two
+    solibs then get loaded at runtime with dlopen(). But this doesn't work
+    with weak symbols: in fact, it seems you end up with the C++ version
+    every time. Boo!
+    
+    So let's be stupider about it and define (bogus) versions of the DPI
+    functions in this case. Fortunately, both of them are designed to
+    return zero on failure so we can just return zero and needn't worry
+    too much.
+    
+    The idea is that when this lands, we can revert the OpenTitan change
+    that switched the C++ code to using weak symbols and Xcelium
+    simulations will start working.
 
 """
 
