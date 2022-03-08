519abd7d79bc9cc0ad67e2237873c1e12b9cf60e
LiteX Robot <robot@timvideos.us>
2022-03-08 14:24:26 +0000

    Updating pythondata-cpu-ibex to 0.0.post2294
    Updated data to v0.0-2168-gaf0c0278 based on af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9 from https://github.com/lowRISC/ibex.
    > commit af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9
    > Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    > Date:   Thu Feb 24 15:03:11 2022 +0000
    >
    >     [ibex, dv] Makes delays between req, gnt and rvalid configurable
    >
    >     This commit adds functionalty to the memory response agent to make delays more
    >     configurable.
    >     There are two delays
    >     - Delay between req and gnt
    >     - Delay between gnt and rvalid
    >
    >     For each of these delays we have three modes:
    >     * Fully random delay
    >     * Fixed delay
    >     * Biased delay. Randomised delays but allow biasing towards 0 delay, to give a mix of runs with back
    >     to back transfers with no delay and some with delays.
    >
    >     Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,63 +4,44 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2292"
-version_tuple = (0, 0, 2292)
+version_str = "0.0.post2294"
+version_tuple = (0, 0, 2294)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2292")
+    pversion = V("0.0.post2294")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2166"
-data_version_tuple = (0, 0, 2166)
+data_version_str = "0.0.post2168"
+data_version_tuple = (0, 0, 2168)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2166")
+    pdata_version = V("0.0.post2168")
 except ImportError:
     pass
-data_git_hash = "c15f3b88883781808eaa96bda205a9bdaff5eb98"
-data_git_describe = "v0.0-2166-gc15f3b88"
+data_git_hash = "af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9"
+data_git_describe = "v0.0-2168-gaf0c0278"
 data_git_msg = """\
-commit c15f3b88883781808eaa96bda205a9bdaff5eb98
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Mar 2 23:46:26 2022 +0000
+commit af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9
+Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+Date:   Thu Feb 24 15:03:11 2022 +0000
 
-    [icache] Define some fake DPI functions to simplify linking
+    [ibex, dv] Makes delays between req, gnt and rvalid configurable
     
-    This is triggered by the fact that if the ICache parameter is false
-    then we don't instantiate the ibex_icache module. For verilator
-    simulations, the module is then discarded entirely, which means that
-    its two DPI functions are not defined. That's unfortunate because
-    we're also compiling the code in scrambled_ecc32_mem_area.cc, which
-    expects the functions to be defined.
+    This commit adds functionalty to the memory response agent to make delays more
+    configurable.
+    There are two delays
+    - Delay between req and gnt
+    - Delay between gnt and rvalid
     
-    The obvious solution (don't include scrambled_ecc32_mem_area.cc if you
-    don't have an icache) isn't easy to do, because FuseSoc doesn't
-    currently allow us to use parameters to configure its dependency
-    tree (see fusesoc issue 438 for a discussion).
+    For each of these delays we have three modes:
+    * Fully random delay
+    * Fixed delay
+    * Biased delay. Randomised delays but allow biasing towards 0 delay, to give a mix of runs with back
+    to back transfers with no delay and some with delays.
     
-    The super-clever solution that I came up with before(!) was to declare
-    these symbols as weak in the C++ code. That way, we can do a runtime
-    check to make sure that no-one is silly enough to call them without an
-    icache, but everything will still build properly either way.
-    
-    Unfortunately, that doesn't work well with xcelium simulations.
-    Xcelium turns out to compile all the C++ code into one .so library and
-    generate functions for exported DPI functions in another. These two
-    solibs then get loaded at runtime with dlopen(). But this doesn't work
-    with weak symbols: in fact, it seems you end up with the C++ version
-    every time. Boo!
-    
-    So let's be stupider about it and define (bogus) versions of the DPI
-    functions in this case. Fortunately, both of them are designed to
-    return zero on failure so we can just return zero and needn't worry
-    too much.
-    
-    The idea is that when this lands, we can revert the OpenTitan change
-    that switched the C++ code to using weak symbols and Xcelium
-    simulations will start working.
+    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
 
 """
 
