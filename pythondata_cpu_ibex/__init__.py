1dd27703c6d1b678eabf0a0b2283e614300dbc7f
LiteX Robot <robot@timvideos.us>
2022-03-09 09:26:43 +0000

    Updating pythondata-cpu-ibex to 0.0.post2297
    Updated data to v0.0-2171-g3438b779 based on 3438b77921941ccbf50cba46b3009a6a37203d4b from https://github.com/lowRISC/ibex.
    > commit 3438b77921941ccbf50cba46b3009a6a37203d4b
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Tue Mar 8 12:07:30 2022 +0000
    >
    >     [rtl] Add minor alert for icache ECC error
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,44 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2294"
-version_tuple = (0, 0, 2294)
+version_str = "0.0.post2297"
+version_tuple = (0, 0, 2297)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2294")
+    pversion = V("0.0.post2297")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2168"
-data_version_tuple = (0, 0, 2168)
+data_version_str = "0.0.post2171"
+data_version_tuple = (0, 0, 2171)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2168")
+    pdata_version = V("0.0.post2171")
 except ImportError:
     pass
-data_git_hash = "af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9"
-data_git_describe = "v0.0-2168-gaf0c0278"
+data_git_hash = "3438b77921941ccbf50cba46b3009a6a37203d4b"
+data_git_describe = "v0.0-2171-g3438b779"
 data_git_msg = """\
-commit af0c0278670225b70d3f9afcb9a6e6ed2e5d8bd9
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Thu Feb 24 15:03:11 2022 +0000
+commit 3438b77921941ccbf50cba46b3009a6a37203d4b
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Tue Mar 8 12:07:30 2022 +0000
 
-    [ibex, dv] Makes delays between req, gnt and rvalid configurable
-    
-    This commit adds functionalty to the memory response agent to make delays more
-    configurable.
-    There are two delays
-    - Delay between req and gnt
-    - Delay between gnt and rvalid
-    
-    For each of these delays we have three modes:
-    * Fully random delay
-    * Fixed delay
-    * Biased delay. Randomised delays but allow biasing towards 0 delay, to give a mix of runs with back
-    to back transfers with no delay and some with delays.
-    
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    [rtl] Add minor alert for icache ECC error
 
 """
 
