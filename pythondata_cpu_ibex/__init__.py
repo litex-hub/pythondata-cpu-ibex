b25997aaff235f308ef9277e649c3cab19101fcf
LiteX Robot <robot@timvideos.us>
2022-03-16 10:29:06 +0000

    Updating pythondata-cpu-ibex to 0.0.post2303
    Updated data to v0.0-2177-gf7724adc based on f7724adcc76bea0bd909c2ac28cf11226d5cbefe from https://github.com/lowRISC/ibex.
    > commit f7724adcc76bea0bd909c2ac28cf11226d5cbefe
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Mar 11 17:10:02 2022 +0000
    >
    >     [rtl] Move memory ECC checks and generation into core
    >
    >     Previously integrity checks for incoming memory reads and integrity
    >     generation for outgoing memory writes were handled within ibex_lockstep
    >     and weren't duplicated.
    >
    >     This moves the integrity checks and generation into the core so they are
    >     replicated and checked as part of the lockstep mechanism.
    >
    >     Additionally it generates a bus error on any memory integrity check
    >     failure. This will result in Ibex taking an exception if any data read
    >     or instruction fetch has bad integrity.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,33 +4,41 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2302"
-version_tuple = (0, 0, 2302)
+version_str = "0.0.post2303"
+version_tuple = (0, 0, 2303)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2302")
+    pversion = V("0.0.post2303")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2176"
-data_version_tuple = (0, 0, 2176)
+data_version_str = "0.0.post2177"
+data_version_tuple = (0, 0, 2177)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2176")
+    pdata_version = V("0.0.post2177")
 except ImportError:
     pass
-data_git_hash = "2f1e188346decdef988ee7cfafc7d44e8d256e7c"
-data_git_describe = "v0.0-2176-g2f1e1883"
+data_git_hash = "f7724adcc76bea0bd909c2ac28cf11226d5cbefe"
+data_git_describe = "v0.0-2177-gf7724adc"
 data_git_msg = """\
-commit 2f1e188346decdef988ee7cfafc7d44e8d256e7c
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Mar 10 12:45:53 2022 +0000
+commit f7724adcc76bea0bd909c2ac28cf11226d5cbefe
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Fri Mar 11 17:10:02 2022 +0000
 
-    Fix port list in top_artya7 example
+    [rtl] Move memory ECC checks and generation into core
     
-    The "alert_major" port was split into "internal" and "bus" parts back
-    in commit 9943f9a. Update the example to match.
+    Previously integrity checks for incoming memory reads and integrity
+    generation for outgoing memory writes were handled within ibex_lockstep
+    and weren't duplicated.
+    
+    This moves the integrity checks and generation into the core so they are
+    replicated and checked as part of the lockstep mechanism.
+    
+    Additionally it generates a bus error on any memory integrity check
+    failure. This will result in Ibex taking an exception if any data read
+    or instruction fetch has bad integrity.
 
 """
 
