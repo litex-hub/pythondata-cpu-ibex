2c0414824336aa6964cba948773477ac19d5e9c9
LiteX Robot <robot@timvideos.us>
2022-03-28 17:29:45 +0000

    Updating pythondata-cpu-ibex to 0.0.post2321
    Updated data to v0.0-2195-g98931c7d based on 98931c7dff32c3177c8b3ad45e558d4f04320ef2 from https://github.com/lowRISC/ibex.
    > commit 98931c7dff32c3177c8b3ad45e558d4f04320ef2
    > Author: Harry Callahan <hcallahan@lowrisc.org>
    > Date:   Mon Mar 28 14:52:44 2022 +0100
    >
    >     Remove logfile param in Ibex RTL Sim for Xcelium
    >
    >     Fixes a bug where both Xcelium and Python open the same sim.log file and race to
    >     write the simulation results into it. This change makes Python the sole writer of this
    >     file using the captured stdout/stderr from the subprocess.run call in
    >     run_rtl.py.
    >
    >     This bug was also previously present for VCS but was fixed in 90ff7ca6c.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,34 +4,37 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2320"
-version_tuple = (0, 0, 2320)
+version_str = "0.0.post2321"
+version_tuple = (0, 0, 2321)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2320")
+    pversion = V("0.0.post2321")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2194"
-data_version_tuple = (0, 0, 2194)
+data_version_str = "0.0.post2195"
+data_version_tuple = (0, 0, 2195)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2194")
+    pdata_version = V("0.0.post2195")
 except ImportError:
     pass
-data_git_hash = "72acfe2fcae98dd4af0deccdfd2c41e04b06d58d"
-data_git_describe = "v0.0-2194-g72acfe2f"
+data_git_hash = "98931c7dff32c3177c8b3ad45e558d4f04320ef2"
+data_git_describe = "v0.0-2195-g98931c7d"
 data_git_msg = """\
-commit 72acfe2fcae98dd4af0deccdfd2c41e04b06d58d
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Mar 25 09:02:58 2022 +0000
+commit 98931c7dff32c3177c8b3ad45e558d4f04320ef2
+Author: Harry Callahan <hcallahan@lowrisc.org>
+Date:   Mon Mar 28 14:52:44 2022 +0100
 
-    [fcov, doc] Update coverage plan
+    Remove logfile param in Ibex RTL Sim for Xcelium
     
-    Added coverpoint and cross names to relevant plan entries so plan is up
-    to date with implemented coverage. Also some minor changes to remove
-    plan entries that are no longer required.
+    Fixes a bug where both Xcelium and Python open the same sim.log file and race to
+    write the simulation results into it. This change makes Python the sole writer of this
+    file using the captured stdout/stderr from the subprocess.run call in
+    run_rtl.py.
+    
+    This bug was also previously present for VCS but was fixed in 90ff7ca6c.
 
 """
 
