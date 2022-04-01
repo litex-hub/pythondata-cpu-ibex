72c67f2dcbe828b8556a9e8db2cf761962e2934f
LiteX Robot <robot@timvideos.us>
2022-04-01 15:32:28 +0000

    Updating pythondata-cpu-ibex to 0.0.post2331
    Updated data to v0.0-2205-g4a2427cd based on 4a2427cd32de1c1ab39b23c4c62270ddb5209a41 from https://github.com/lowRISC/ibex.
    > commit 4a2427cd32de1c1ab39b23c4c62270ddb5209a41
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Fri Apr 1 14:20:16 2022 +0100
    >
    >     Fix cov_report directory in sim.py
    >
    >     Enables us to save coverage groups text file which helps easily see the overall
    >     coverage in a regression.
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,52 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2329"
-version_tuple = (0, 0, 2329)
+version_str = "0.0.post2331"
+version_tuple = (0, 0, 2331)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2329")
+    pversion = V("0.0.post2331")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2203"
-data_version_tuple = (0, 0, 2203)
+data_version_str = "0.0.post2205"
+data_version_tuple = (0, 0, 2205)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2203")
+    pdata_version = V("0.0.post2205")
 except ImportError:
     pass
-data_git_hash = "fe3e029108d07aa6f032ad4a6c972c9609907d9e"
-data_git_describe = "v0.0-2203-gfe3e0291"
+data_git_hash = "4a2427cd32de1c1ab39b23c4c62270ddb5209a41"
+data_git_describe = "v0.0-2205-g4a2427cd"
 data_git_msg = """\
-commit fe3e029108d07aa6f032ad4a6c972c9609907d9e
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Apr 1 15:34:53 2022 +0200
+commit 4a2427cd32de1c1ab39b23c4c62270ddb5209a41
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Fri Apr 1 14:20:16 2022 +0100
 
-    Update google_riscv-dv to google/riscv-dv@cb4295f
+    Fix cov_report directory in sim.py
     
-    Update code from upstream repository https://github.com/google/riscv-
-    dv to revision cb4295f9ce5da2881d7746015a6105adb8f09071
-    
-    * Update list search (Matthew Ballance)
-    * Trap and report exceptions encountered in sub-processes and
-      propagate error back (Matthew Ballance)
-    * Workaround fix for loop test colon issue (aneels3)
-    * Fix typo (aneels3)
-    * Add support for RV64AFD (aneels3)
-    * Fix typo (aneels3)
-    * Update README.md (aneels3)
-    * Add support for sub_programs (aneels3)
-    * fix issue with imm value for 64 bit instr (aneels3)
-    * Allow for underscores and capital letters in ISA for ISS (Pirmin
-      Vogel)
-    * implement rv64i (shrujal20)
-    * Add support for RV32FD coverage (aneels3)
-    * Integrate random seed for pyflow (aneels3)
-    * Add riscv_loop_test (ShraddhaDevaiya)
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Enables us to save coverage groups text file which helps easily see the overall
+    coverage in a regression.
 
 """
 
