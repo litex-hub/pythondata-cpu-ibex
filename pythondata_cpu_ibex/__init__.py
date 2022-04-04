ba61349f2bd542e93237138ff678c8a9ba219e4a
LiteX Robot <robot@timvideos.us>
2022-04-04 15:33:56 +0000

    Updating pythondata-cpu-ibex to 0.0.post2337
    Updated data to v0.0-2211-ga3e5eebf based on a3e5eebffa9eb1aed3d04f229299aa7da8e7195e from https://github.com/lowRISC/ibex.
    > commit a3e5eebffa9eb1aed3d04f229299aa7da8e7195e
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Mon Apr 4 10:22:46 2022 +0100
    >
    >     [dv,fcov] Timeout fix + removing .ccf from yaml
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2334"
-version_tuple = (0, 0, 2334)
+version_str = "0.0.post2337"
+version_tuple = (0, 0, 2337)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2334")
+    pversion = V("0.0.post2337")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2208"
-data_version_tuple = (0, 0, 2208)
+data_version_str = "0.0.post2211"
+data_version_tuple = (0, 0, 2211)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2208")
+    pdata_version = V("0.0.post2211")
 except ImportError:
     pass
-data_git_hash = "a3b50fb69420f3534a6cd81ba196d4252100a542"
-data_git_describe = "v0.0-2208-ga3b50fb6"
+data_git_hash = "a3e5eebffa9eb1aed3d04f229299aa7da8e7195e"
+data_git_describe = "v0.0-2211-ga3e5eebf"
 data_git_msg = """\
-commit a3b50fb69420f3534a6cd81ba196d4252100a542
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Apr 1 17:38:48 2022 +0100
+commit a3e5eebffa9eb1aed3d04f229299aa7da8e7195e
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Mon Apr 4 10:22:46 2022 +0100
 
-    [doc, fcov] Remove coverpoint names from unimplemented coverage
+    [dv,fcov] Timeout fix + removing .ccf from yaml
+    
+    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
 """
 
