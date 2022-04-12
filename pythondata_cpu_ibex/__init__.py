cbee79d6966d1eea6726ed573ccb96a5b54f55c4
LiteX Robot <robot@timvideos.us>
2022-04-12 17:31:25 +0000

    Updating pythondata-cpu-ibex to 0.0.post2345
    Updated data to v0.0-2219-g6caf82c2 based on 6caf82c2b402c4b0895a514ca30bc15f4705e17f from https://github.com/lowRISC/ibex.
    > commit 6caf82c2b402c4b0895a514ca30bc15f4705e17f
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Mon Apr 11 18:59:38 2022 -0700
    >
    >     [lint] Minor fixes
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2344"
-version_tuple = (0, 0, 2344)
+version_str = "0.0.post2345"
+version_tuple = (0, 0, 2345)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2344")
+    pversion = V("0.0.post2345")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2218"
-data_version_tuple = (0, 0, 2218)
+data_version_str = "0.0.post2219"
+data_version_tuple = (0, 0, 2219)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2218")
+    pdata_version = V("0.0.post2219")
 except ImportError:
     pass
-data_git_hash = "2ae62c145ca2d798d29faaf16f7b171c776430dd"
-data_git_describe = "v0.0-2218-g2ae62c14"
+data_git_hash = "6caf82c2b402c4b0895a514ca30bc15f4705e17f"
+data_git_describe = "v0.0-2219-g6caf82c2"
 data_git_msg = """\
-commit 2ae62c145ca2d798d29faaf16f7b171c776430dd
-Author: mbaykenar <aykenar.burak@gmail.com>
-Date:   Tue Apr 12 16:37:08 2022 +0300
+commit 6caf82c2b402c4b0895a514ca30bc15f4705e17f
+Author: Michael Schaffner <msf@google.com>
+Date:   Mon Apr 11 18:59:38 2022 -0700
 
-    assign irq_nm_int_cause to all zeros
+    [lint] Minor fixes
+    
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
