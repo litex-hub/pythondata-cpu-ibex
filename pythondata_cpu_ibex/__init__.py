3dec54459c91b08f30f475ee92480b338822787e
LiteX Robot <robot@timvideos.us>
2022-04-12 10:32:45 +0000

    Updating pythondata-cpu-ibex to 0.0.post2342
    Updated data to v0.0-2216-g1f6bcb9a based on 1f6bcb9ab5029225b606bf42ff3b61b2fddeed2b from https://github.com/lowRISC/ibex.
    > commit 1f6bcb9ab5029225b606bf42ff3b61b2fddeed2b
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Mar 24 16:16:56 2022 +0000
    >
    >     [dv] Remove support for building against ibex-cosim-v0.1
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,34 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2340"
-version_tuple = (0, 0, 2340)
+version_str = "0.0.post2342"
+version_tuple = (0, 0, 2342)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2340")
+    pversion = V("0.0.post2342")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2214"
-data_version_tuple = (0, 0, 2214)
+data_version_str = "0.0.post2216"
+data_version_tuple = (0, 0, 2216)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2214")
+    pdata_version = V("0.0.post2216")
 except ImportError:
     pass
-data_git_hash = "0a9f5ed1daa4a7fcf46f2e21cbb02d87e569ea1a"
-data_git_describe = "v0.0-2214-g0a9f5ed1"
+data_git_hash = "1f6bcb9ab5029225b606bf42ff3b61b2fddeed2b"
+data_git_describe = "v0.0-2216-g1f6bcb9a"
 data_git_msg = """\
-commit 0a9f5ed1daa4a7fcf46f2e21cbb02d87e569ea1a
+commit 1f6bcb9ab5029225b606bf42ff3b61b2fddeed2b
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Mar 21 16:15:08 2022 +0000
+Date:   Thu Mar 24 16:16:56 2022 +0000
 
-    [rtl] Remove "mispredict" ports from icache
-    
-    These are no longer needed: a previous commit has moved the logic that
-    handles misprediction into the IF stage and branch_mispredict_i was
-    dead zero.
+    [dv] Remove support for building against ibex-cosim-v0.1
 
 """
 
