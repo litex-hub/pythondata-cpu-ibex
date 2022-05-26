f2166da489140533b33159f6d47ea54805ce0302
LiteX Robot <robot@timvideos.us>
2022-05-26 17:05:18 +0000

    Updating pythondata-cpu-ibex to 0.0.post2449
    Updated data to v0.0-2311-g97a949df based on 97a949df0283fbae223372f592287f275ae869f7 from https://github.com/lowRISC/ibex.
    > commit 97a949df0283fbae223372f592287f275ae869f7
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Tue May 17 16:46:58 2022 +0100
    >
    >     [doc] Update coverage plan to point crosses/cp's
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post138 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2447"
-version_tuple = (0, 0, 2447)
+version_str = "0.0.post2449"
+version_tuple = (0, 0, 2449)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2447")
+    pversion = V("0.0.post2449")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Tue May 17 16:46:58 2022 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post136"
-tool_version_tuple = (0, 0, 136)
+tool_version_str = "0.0.post138"
+tool_version_tuple = (0, 0, 138)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post136")
+    ptool_version = V("0.0.post138")
 except ImportError:
     pass
 
