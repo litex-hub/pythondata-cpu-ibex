4fd7289d0256daea927443ee6b8e1494f0a38495
LiteX Robot <robot@timvideos.us>
2022-07-19 10:12:49 +0000

    Updating pythondata-cpu-ibex to 0.0.post2470
    Updated data to v0.0-2328-g51f11413 based on 51f114133514bb6205411ff1f9eed5791c73879e from https://github.com/lowRISC/ibex.
    > commit 51f114133514bb6205411ff1f9eed5791c73879e
    > Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
    > Date:   Fri Jul 15 11:33:47 2022 +0100
    >
    >     [lint] Point to correct Verible rules for lint workflow
    >
    >     Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2469"
-version_tuple = (0, 0, 2469)
+version_str = "0.0.post2470"
+version_tuple = (0, 0, 2470)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2469")
+    pversion = V("0.0.post2470")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2327"
-data_version_tuple = (0, 0, 2327)
+data_version_str = "0.0.post2328"
+data_version_tuple = (0, 0, 2328)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2327")
+    pdata_version = V("0.0.post2328")
 except ImportError:
     pass
-data_git_hash = "42d92c7c9bb96a63cdaec78cd40f1367d2eadbd3"
-data_git_describe = "v0.0-2327-g42d92c7c"
+data_git_hash = "51f114133514bb6205411ff1f9eed5791c73879e"
+data_git_describe = "v0.0-2328-g51f11413"
 data_git_msg = """\
-commit 42d92c7c9bb96a63cdaec78cd40f1367d2eadbd3
-Author: Harry Callahan <hcallahan@lowrisc.org>
-Date:   Thu Jun 30 14:13:52 2022 +0100
+commit 51f114133514bb6205411ff1f9eed5791c73879e
+Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
+Date:   Fri Jul 15 11:33:47 2022 +0100
 
-    Create riscv_assorted_traps_interrupts_debug_test
+    [lint] Point to correct Verible rules for lint workflow
     
-    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>
+    Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>
 
 """
 
