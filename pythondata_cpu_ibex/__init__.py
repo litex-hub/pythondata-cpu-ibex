bdcc05bfb2913252fd0b5388af1a1c10b87f869e
LiteX Robot <robot@timvideos.us>
2022-07-20 22:13:37 +0000

    Updating pythondata-cpu-ibex to 0.0.post2472
    Updated data to v0.0-2330-gdfca76f3 based on dfca76f3865d9fc8ab7f093ff563f91add6648ca from https://github.com/lowRISC/ibex.
    > commit dfca76f3865d9fc8ab7f093ff563f91add6648ca
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Tue Jul 12 23:44:19 2022 +0100
    >
    >     [dv,fcov] Implement Misaligned Mem Error coverage
    >
    >     Adds some signal to the load store unit to catch when we have the
    >     fetch error signals from both first and second part of the misaligned
    >     load/store access cases.
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2471"
-version_tuple = (0, 0, 2471)
+version_str = "0.0.post2472"
+version_tuple = (0, 0, 2472)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2471")
+    pversion = V("0.0.post2472")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2329"
-data_version_tuple = (0, 0, 2329)
+data_version_str = "0.0.post2330"
+data_version_tuple = (0, 0, 2330)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2329")
+    pdata_version = V("0.0.post2330")
 except ImportError:
     pass
-data_git_hash = "1ea89a423b314f53840510edadb4ef3487281f2d"
-data_git_describe = "v0.0-2329-g1ea89a42"
+data_git_hash = "dfca76f3865d9fc8ab7f093ff563f91add6648ca"
+data_git_describe = "v0.0-2330-gdfca76f3"
 data_git_msg = """\
-commit 1ea89a423b314f53840510edadb4ef3487281f2d
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Jul 19 16:06:21 2022 +0100
+commit dfca76f3865d9fc8ab7f093ff563f91add6648ca
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Tue Jul 12 23:44:19 2022 +0100
 
-    [dv] Fix traps in simple system cosim
+    [dv,fcov] Implement Misaligned Mem Error coverage
     
-    Previously any traps seen on RVFI were skipped over. This was old
-    behaviour. With the latest cosim setup traps must be passed to the
-    `step` function.
+    Adds some signal to the load store unit to catch when we have the
+    fetch error signals from both first and second part of the misaligned
+    load/store access cases.
+    
+    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
 """
 
