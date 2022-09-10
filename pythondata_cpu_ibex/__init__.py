bd7f1d0f7d4330db849d795f81b3a9bb49c54135
LiteX Robot <robot@timvideos.us>
2022-09-10 04:33:55 +0000

    Updating pythondata-cpu-ibex to 0.0.post2553
    Updated data to v0.0-2411-g02ccf9e5 based on 02ccf9e5d066fbb8132ba712314558da974c3601 from https://github.com/lowRISC/ibex.
    > commit 02ccf9e5d066fbb8132ba712314558da974c3601
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed Sep 7 13:01:43 2022 +0100
    >
    >     [ci] Bump cosim version for privilege spec updates
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,36 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2486"
-version_tuple = (0, 0, 2486)
+version_str = "0.0.post2553"
+version_tuple = (0, 0, 2553)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2486")
+    pversion = V("0.0.post2553")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2344"
-data_version_tuple = (0, 0, 2344)
+data_version_str = "0.0.post2411"
+data_version_tuple = (0, 0, 2411)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2344")
+    pdata_version = V("0.0.post2411")
 except ImportError:
     pass
-data_git_hash = "6dc0683773635bfde3949c6fa7d9c3e7282ab916"
-data_git_describe = "v0.0-2344-g6dc06837"
+data_git_hash = "02ccf9e5d066fbb8132ba712314558da974c3601"
+data_git_describe = "v0.0-2411-g02ccf9e5"
 data_git_msg = """\
-commit 6dc0683773635bfde3949c6fa7d9c3e7282ab916
+commit 02ccf9e5d066fbb8132ba712314558da974c3601
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Jul 26 13:28:44 2022 +0100
+Date:   Wed Sep 7 13:01:43 2022 +0100
 
-    [dv] Fix IbexDataRPayloadX assertion
-    
-    Some aspects of the memory response are only relevant to reads. This
-    introduces outstanding request tracking so we know which outstanding
-    requests are reads and applies X checks appropriately.
-    
-    Fixes #1645
+    [ci] Bump cosim version for privilege spec updates
 
 """
 
