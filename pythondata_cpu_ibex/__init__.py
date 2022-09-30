5ee10359507dc059c1b0503928c38ea0d270a1c4
LiteX Robot <robot@timvideos.us>
2022-09-30 10:17:50 +0000

    Updating pythondata-cpu-ibex to 0.0.post2562
    Updated data to v0.0-2420-g3943a4ec based on 3943a4eca3917d0a00406abb36f4951304c0b394 from https://github.com/lowRISC/ibex.
    > commit 3943a4eca3917d0a00406abb36f4951304c0b394
    > Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
    > Date:   Tue Sep 27 11:36:50 2022 +0100
    >
    >     [pmp] Remove off mode from pmp_*_mode_cross coverpoints
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,33 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2561"
-version_tuple = (0, 0, 2561)
+version_str = "0.0.post2562"
+version_tuple = (0, 0, 2562)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2561")
+    pversion = V("0.0.post2562")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2419"
-data_version_tuple = (0, 0, 2419)
+data_version_str = "0.0.post2420"
+data_version_tuple = (0, 0, 2420)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2419")
+    pdata_version = V("0.0.post2420")
 except ImportError:
     pass
-data_git_hash = "1313104bade1302ee651610b8536209255654cc4"
-data_git_describe = "v0.0-2419-g1313104b"
+data_git_hash = "3943a4eca3917d0a00406abb36f4951304c0b394"
+data_git_describe = "v0.0-2420-g3943a4ec"
 data_git_msg = """\
-commit 1313104bade1302ee651610b8536209255654cc4
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Wed Sep 28 11:59:48 2022 +0100
+commit 3943a4eca3917d0a00406abb36f4951304c0b394
+Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
+Date:   Tue Sep 27 11:36:50 2022 +0100
 
-    [ci] Fix pmp_smoke_test
-    
-    It was renamed pmp_smoke_test from pmp_exception_test in the software
-    build but not the actual test run
+    [pmp] Remove off mode from pmp_*_mode_cross coverpoints
 
 """
 
