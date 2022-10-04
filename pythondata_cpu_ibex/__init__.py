9a48357fae1fb87f0709747a615ce00f423df50f
LiteX Robot <robot@timvideos.us>
2022-10-04 13:52:07 +0000

    Updating pythondata-cpu-ibex to 0.0.post2566
    Updated data to v0.0-2424-g1cdd4035 based on 1cdd403564f4b2e4352919f591ee9182cebcfb67 from https://github.com/lowRISC/ibex.
    > commit 1cdd403564f4b2e4352919f591ee9182cebcfb67
    > Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
    > Date:   Tue Oct 4 10:20:13 2022 +0100
    >
    >     [formal] Remove build infrastructure for instruction cache assertions
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2563"
-version_tuple = (0, 0, 2563)
+version_str = "0.0.post2566"
+version_tuple = (0, 0, 2566)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2563")
+    pversion = V("0.0.post2566")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2421"
-data_version_tuple = (0, 0, 2421)
+data_version_str = "0.0.post2424"
+data_version_tuple = (0, 0, 2424)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2421")
+    pdata_version = V("0.0.post2424")
 except ImportError:
     pass
-data_git_hash = "43dc5e85726908cedbc4f3b6798b6ebd8dd1b720"
-data_git_describe = "v0.0-2421-g43dc5e85"
+data_git_hash = "1cdd403564f4b2e4352919f591ee9182cebcfb67"
+data_git_describe = "v0.0-2424-g1cdd4035"
 data_git_msg = """\
-commit 43dc5e85726908cedbc4f3b6798b6ebd8dd1b720
+commit 1cdd403564f4b2e4352919f591ee9182cebcfb67
 Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
-Date:   Mon Oct 3 15:25:49 2022 +0100
+Date:   Tue Oct 4 10:20:13 2022 +0100
 
-    [formal] Added missing prim secded package
+    [formal] Remove build infrastructure for instruction cache assertions
 
 """
 
