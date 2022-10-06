baeeb2ad57877892663875de193380b751118712
LiteX Robot <robot@timvideos.us>
2022-10-06 17:29:27 +0000

    Updating pythondata-cpu-ibex to 0.0.post2573
    Updated data to v0.0-2431-g3c11ef10 based on 3c11ef10b98a73c06345bd0f2fcb8d94226b4f31 from https://github.com/lowRISC/ibex.
    > commit 3c11ef10b98a73c06345bd0f2fcb8d94226b4f31
    > Author: Harry Callahan <hcallahan@lowrisc.org>
    > Date:   Thu Sep 29 17:42:47 2022 +0100
    >
    >     single_step test : only drive debug_req_i after stepping finishes
    >
    >     This addresses a current testbench issue where asserting debug_req_i close to
    >     when single_stepping over an instruction causes an incorrect 'cause' to be
    >     recorded within DCSR.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2571"
-version_tuple = (0, 0, 2571)
+version_str = "0.0.post2573"
+version_tuple = (0, 0, 2573)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2571")
+    pversion = V("0.0.post2573")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2429"
-data_version_tuple = (0, 0, 2429)
+data_version_str = "0.0.post2431"
+data_version_tuple = (0, 0, 2431)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2429")
+    pdata_version = V("0.0.post2431")
 except ImportError:
     pass
-data_git_hash = "7ab2571bea0daca3f14155d82f0124fb0b118799"
-data_git_describe = "v0.0-2429-g7ab2571b"
+data_git_hash = "3c11ef10b98a73c06345bd0f2fcb8d94226b4f31"
+data_git_describe = "v0.0-2431-g3c11ef10"
 data_git_msg = """\
-commit 7ab2571bea0daca3f14155d82f0124fb0b118799
-Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
-Date:   Wed Oct 5 18:28:35 2022 +0100
+commit 3c11ef10b98a73c06345bd0f2fcb8d94226b4f31
+Author: Harry Callahan <hcallahan@lowrisc.org>
+Date:   Thu Sep 29 17:42:47 2022 +0100
 
-    [if,pmp] Check second bit instead of third for instruction alignment
+    single_step test : only drive debug_req_i after stepping finishes
     
-    Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>
+    This addresses a current testbench issue where asserting debug_req_i close to
+    when single_stepping over an instruction causes an incorrect 'cause' to be
+    recorded within DCSR.
 
 """
 
