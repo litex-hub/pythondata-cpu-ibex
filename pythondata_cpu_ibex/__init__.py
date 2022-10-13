e11bfeeb8d66b402260eba1ebbd78b375338bbc1
LiteX Robot <robot@timvideos.us>
2022-10-13 17:37:33 +0000

    Updating pythondata-cpu-ibex to 0.0.post2584
    Updated data to v0.0-2442-g48733e23 based on 48733e23ec19e68bd334f27fd9b06f667a012ca3 from https://github.com/lowRISC/ibex.
    > commit 48733e23ec19e68bd334f27fd9b06f667a012ca3
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Sep 30 17:45:38 2022 +0100
    >
    >     [rtl] Ignore MIE bit in U mode
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,38 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2582"
-version_tuple = (0, 0, 2582)
+version_str = "0.0.post2584"
+version_tuple = (0, 0, 2584)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2582")
+    pversion = V("0.0.post2584")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2440"
-data_version_tuple = (0, 0, 2440)
+data_version_str = "0.0.post2442"
+data_version_tuple = (0, 0, 2442)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2440")
+    pdata_version = V("0.0.post2442")
 except ImportError:
     pass
-data_git_hash = "25d81afef664351f6fd14fd4ed7a19bba2224b55"
-data_git_describe = "v0.0-2440-g25d81afe"
+data_git_hash = "48733e23ec19e68bd334f27fd9b06f667a012ca3"
+data_git_describe = "v0.0-2442-g48733e23"
 data_git_msg = """\
-commit 25d81afef664351f6fd14fd4ed7a19bba2224b55
-Author: Harry Callahan <hcallahan@lowrisc.org>
-Date:   Tue Oct 11 16:50:16 2022 +0100
+commit 48733e23ec19e68bd334f27fd9b06f667a012ca3
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Fri Sep 30 17:45:38 2022 +0100
 
-    Update google_riscv-dv to google/riscv-dv@c6acc18
-    
-    Update code from upstream repository https://github.com/google/riscv-
-    dv to revision c6acc1897429f5245cc89b2ecee2e3eefdefd18d
-    
-    * Add plusarg to enable ECALL insn in main randomized body (Harry
-      Callahan)
-    
-    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>
+    [rtl] Ignore MIE bit in U mode
 
 """
 
