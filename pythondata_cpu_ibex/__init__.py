c4d5f5bdbc06bf7ed1774fbc4862fa3efdfa8ce7
LiteX Robot <robot@timvideos.us>
2022-10-14 16:20:02 +0000

    Updating pythondata-cpu-ibex to 0.0.post2586
    Updated data to v0.0-2444-gd7ce0827 based on d7ce0827790bc2b551f55670103c516257a4ca8c from https://github.com/lowRISC/ibex.
    > commit d7ce0827790bc2b551f55670103c516257a4ca8c
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Oct 14 08:29:07 2022 +0100
    >
    >     [dv] Remove riscv_perf_counter_test
    >
    >     This test doesn't actually check the performance counters, it just runs
    >     a random instruction test and dumps the performance counters at the end
    >     for some final checking. That checking does not exist. The test is
    >     currently broken as well so just remove it as it adds nothing to the
    >     regression.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,36 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2584"
-version_tuple = (0, 0, 2584)
+version_str = "0.0.post2586"
+version_tuple = (0, 0, 2586)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2584")
+    pversion = V("0.0.post2586")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2442"
-data_version_tuple = (0, 0, 2442)
+data_version_str = "0.0.post2444"
+data_version_tuple = (0, 0, 2444)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2442")
+    pdata_version = V("0.0.post2444")
 except ImportError:
     pass
-data_git_hash = "48733e23ec19e68bd334f27fd9b06f667a012ca3"
-data_git_describe = "v0.0-2442-g48733e23"
+data_git_hash = "d7ce0827790bc2b551f55670103c516257a4ca8c"
+data_git_describe = "v0.0-2444-gd7ce0827"
 data_git_msg = """\
-commit 48733e23ec19e68bd334f27fd9b06f667a012ca3
+commit d7ce0827790bc2b551f55670103c516257a4ca8c
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Sep 30 17:45:38 2022 +0100
+Date:   Fri Oct 14 08:29:07 2022 +0100
 
-    [rtl] Ignore MIE bit in U mode
+    [dv] Remove riscv_perf_counter_test
+    
+    This test doesn't actually check the performance counters, it just runs
+    a random instruction test and dumps the performance counters at the end
+    for some final checking. That checking does not exist. The test is
+    currently broken as well so just remove it as it adds nothing to the
+    regression.
 
 """
 
