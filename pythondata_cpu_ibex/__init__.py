008402f80812e894fc3addd941d067137de010fa
LiteX Robot <robot@timvideos.us>
2022-10-16 16:17:48 +0000

    Updating pythondata-cpu-ibex to 0.0.post2593
    Updated data to v0.0-2451-g083fe2a5 based on 083fe2a54f087235a09f4c90bc1560d798b9685e from https://github.com/lowRISC/ibex.
    > commit 083fe2a54f087235a09f4c90bc1560d798b9685e
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Sat Oct 15 20:36:49 2022 +0100
    >
    >     [dv] Use fetch enable sequence by default
    >
    >     This sequence randomly toggles the fetch enable.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,32 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2589"
-version_tuple = (0, 0, 2589)
+version_str = "0.0.post2593"
+version_tuple = (0, 0, 2593)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2589")
+    pversion = V("0.0.post2593")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2447"
-data_version_tuple = (0, 0, 2447)
+data_version_str = "0.0.post2451"
+data_version_tuple = (0, 0, 2451)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2447")
+    pdata_version = V("0.0.post2451")
 except ImportError:
     pass
-data_git_hash = "75a93dbed0dbe340b637c82e2a8e3ba1b841ecc3"
-data_git_describe = "v0.0-2447-g75a93dbe"
+data_git_hash = "083fe2a54f087235a09f4c90bc1560d798b9685e"
+data_git_describe = "v0.0-2451-g083fe2a5"
 data_git_msg = """\
-commit 75a93dbed0dbe340b637c82e2a8e3ba1b841ecc3
-Author: Harry Callahan <hcallahan@lowrisc.org>
-Date:   Tue Oct 11 14:29:36 2022 +0100
+commit 083fe2a54f087235a09f4c90bc1560d798b9685e
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Sat Oct 15 20:36:49 2022 +0100
 
-    Fixup signal used when checking for ebreak cause
+    [dv] Use fetch enable sequence by default
+    
+    This sequence randomly toggles the fetch enable.
 
 """
 
