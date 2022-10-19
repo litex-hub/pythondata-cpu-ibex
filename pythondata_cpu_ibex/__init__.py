1dd5db454ef7fee5329fe477f35d15ce99e4dd10
LiteX Robot <robot@timvideos.us>
2022-10-19 09:25:48 +0000

    Updating pythondata-cpu-ibex to 0.0.post2598
    Updated data to v0.0-2456-g73e46b4f based on 73e46b4fc702e5ab31cbd8e4446ec9e6ce756abc from https://github.com/lowRISC/ibex.
    > commit 73e46b4fc702e5ab31cbd8e4446ec9e6ce756abc
    > Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
    > Date:   Tue Oct 18 18:00:48 2022 +0100
    >
    >     [fcov,pmp] Illegal PMP write coverpoints check dside request error not low
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,37 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2597"
-version_tuple = (0, 0, 2597)
+version_str = "0.0.post2598"
+version_tuple = (0, 0, 2598)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2597")
+    pversion = V("0.0.post2598")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2455"
-data_version_tuple = (0, 0, 2455)
+data_version_str = "0.0.post2456"
+data_version_tuple = (0, 0, 2456)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2455")
+    pdata_version = V("0.0.post2456")
 except ImportError:
     pass
-data_git_hash = "33f1d0a702bac8f1fbd18bda0c447a0329d1d6d3"
-data_git_describe = "v0.0-2455-g33f1d0a7"
+data_git_hash = "73e46b4fc702e5ab31cbd8e4446ec9e6ce756abc"
+data_git_describe = "v0.0-2456-g73e46b4f"
 data_git_msg = """\
-commit 33f1d0a702bac8f1fbd18bda0c447a0329d1d6d3
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Fri Oct 14 16:16:52 2022 +0100
+commit 73e46b4fc702e5ab31cbd8e4446ec9e6ce756abc
+Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
+Date:   Tue Oct 18 18:00:48 2022 +0100
 
-    Update google_riscv-dv to google/riscv-dv@e0eae9e
-    
-    Update code from upstream repository https://github.com/google/riscv-
-    dv to revision e0eae9e0ca69770c519c82c48421005f65521eac
-    
-    * [sv] Explicit type casting for VCS compability (Canberk Topal)
-    
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    [fcov,pmp] Illegal PMP write coverpoints check dside request error not low
 
 """
 
