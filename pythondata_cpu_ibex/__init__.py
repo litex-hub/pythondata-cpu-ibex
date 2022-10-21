763e03029522d8732fdf71d252c83cac38154af9
LiteX Robot <robot@timvideos.us>
2022-10-21 16:24:59 +0000

    Updating pythondata-cpu-ibex to 0.0.post2604
    Updated data to v0.0-2462-ge38f534a based on e38f534ac249128ce2c2949ba779c82504d5fe4c from https://github.com/lowRISC/ibex.
    > commit e38f534ac249128ce2c2949ba779c82504d5fe4c
    > Author: Harry Callahan <hcallahan@lowrisc.org>
    > Date:   Wed Oct 19 18:24:37 2022 +0100
    >
    >     Add wall-clock timeout within rtl simulation to gracefully end
    >
    >     Use a DPI call to unix 'date' to implement a wall-clock timeout entirely within
    >     a simulation. This allows the UVM environment to gracefully end when the
    >     threshold is reached, and for things like logs and coverage databases to be
    >     generated correctly.
    >     Previously, a process-level timeout was used, which gave the running simulation
    >     no time to commit any logs/databases to disk before ending. Hence we would not
    >     gather any coverage from timed-out tests.
    >
    >     A plusarg 'test_timeout_s' can be specified to each test to set the timeout. The
    >     default timeout is 1800s.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,41 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2600"
-version_tuple = (0, 0, 2600)
+version_str = "0.0.post2604"
+version_tuple = (0, 0, 2604)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2600")
+    pversion = V("0.0.post2604")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2458"
-data_version_tuple = (0, 0, 2458)
+data_version_str = "0.0.post2462"
+data_version_tuple = (0, 0, 2462)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2458")
+    pdata_version = V("0.0.post2462")
 except ImportError:
     pass
-data_git_hash = "a376f85f26df52cd4ae0767fbebfdaa0dd0d0e07"
-data_git_describe = "v0.0-2458-ga376f85f"
+data_git_hash = "e38f534ac249128ce2c2949ba779c82504d5fe4c"
+data_git_describe = "v0.0-2462-ge38f534a"
 data_git_msg = """\
-commit a376f85f26df52cd4ae0767fbebfdaa0dd0d0e07
-Author: Marno van der Maas <mvdmaas+git@lowrisc.org>
-Date:   Fri Oct 14 11:22:50 2022 +0100
+commit e38f534ac249128ce2c2949ba779c82504d5fe4c
+Author: Harry Callahan <hcallahan@lowrisc.org>
+Date:   Wed Oct 19 18:24:37 2022 +0100
 
-    [lint] Shellcheck bash scripts in repo
+    Add wall-clock timeout within rtl simulation to gracefully end
+    
+    Use a DPI call to unix 'date' to implement a wall-clock timeout entirely within
+    a simulation. This allows the UVM environment to gracefully end when the
+    threshold is reached, and for things like logs and coverage databases to be
+    generated correctly.
+    Previously, a process-level timeout was used, which gave the running simulation
+    no time to commit any logs/databases to disk before ending. Hence we would not
+    gather any coverage from timed-out tests.
+    
+    A plusarg 'test_timeout_s' can be specified to each test to set the timeout. The
+    default timeout is 1800s.
 
 """
 
