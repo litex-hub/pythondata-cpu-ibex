2eb6eadca2c090fe6adfdf0ebd35566293cd04e1
LiteX Robot <robot@timvideos.us>
2022-10-26 11:13:56 +0000

    Updating pythondata-cpu-ibex to 0.0.post2613
    Updated data to v0.0-2471-g1d4cf9b2 based on 1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1 from https://github.com/lowRISC/ibex.
    > commit 1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Tue Oct 25 17:36:31 2022 +0100
    >
    >     [dv] Add single step over exception coverpoint
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,33 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2610"
-version_tuple = (0, 0, 2610)
+version_str = "0.0.post2613"
+version_tuple = (0, 0, 2613)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2610")
+    pversion = V("0.0.post2613")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2468"
-data_version_tuple = (0, 0, 2468)
+data_version_str = "0.0.post2471"
+data_version_tuple = (0, 0, 2471)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2468")
+    pdata_version = V("0.0.post2471")
 except ImportError:
     pass
-data_git_hash = "659dc458f2a8d32cbb04ca4b43f793f51faf1ee8"
-data_git_describe = "v0.0-2468-g659dc458"
+data_git_hash = "1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1"
+data_git_describe = "v0.0-2471-g1d4cf9b2"
 data_git_msg = """\
-commit 659dc458f2a8d32cbb04ca4b43f793f51faf1ee8
-Author: Harry Callahan <hcallahan@lowrisc.org>
-Date:   Tue Oct 25 14:46:50 2022 +0100
+commit 1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Tue Oct 25 17:36:31 2022 +0100
 
-    Fix bug in passing cosim_agent handle to the data_intf_seq
-    
-    The handle was passed before the cosim was constructed, so when it came to use
-    the handle it caused a null pointer exception.
+    [dv] Add single step over exception coverpoint
 
 """
 
