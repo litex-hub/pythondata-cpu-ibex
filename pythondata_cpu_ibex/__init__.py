b509114d197cdf4cdc63605425ffdfbf9854d7da
LiteX Robot <robot@timvideos.us>
2022-10-26 21:14:20 +0000

    Updating pythondata-cpu-ibex to 0.0.post2614
    Updated data to v0.0-2472-gbbda68a0 based on bbda68a0df2f896e920e820a2f759f6855b34148 from https://github.com/lowRISC/ibex.
    > commit bbda68a0df2f896e920e820a2f759f6855b34148
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Tue Oct 25 19:29:33 2022 +0100
    >
    >     [dv] Disable bad integrity on uninitialised memory for selected tests
    >
    >     From an initial triage and test regression run these tests benefit from
    >     this.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,30 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2613"
-version_tuple = (0, 0, 2613)
+version_str = "0.0.post2614"
+version_tuple = (0, 0, 2614)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2613")
+    pversion = V("0.0.post2614")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2471"
-data_version_tuple = (0, 0, 2471)
+data_version_str = "0.0.post2472"
+data_version_tuple = (0, 0, 2472)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2471")
+    pdata_version = V("0.0.post2472")
 except ImportError:
     pass
-data_git_hash = "1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1"
-data_git_describe = "v0.0-2471-g1d4cf9b2"
+data_git_hash = "bbda68a0df2f896e920e820a2f759f6855b34148"
+data_git_describe = "v0.0-2472-gbbda68a0"
 data_git_msg = """\
-commit 1d4cf9b207467eb37cbcd7c20ffcdd55cc7077e1
+commit bbda68a0df2f896e920e820a2f759f6855b34148
 Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Oct 25 17:36:31 2022 +0100
+Date:   Tue Oct 25 19:29:33 2022 +0100
 
-    [dv] Add single step over exception coverpoint
+    [dv] Disable bad integrity on uninitialised memory for selected tests
+    
+    From an initial triage and test regression run these tests benefit from
+    this.
 
 """
 
