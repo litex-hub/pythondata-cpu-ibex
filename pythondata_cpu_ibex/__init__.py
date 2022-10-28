a142f2256839db0ff16eda2c2154303af70d6592
LiteX Robot <robot@timvideos.us>
2022-10-28 17:22:01 +0000

    Updating pythondata-cpu-ibex to 0.0.post2624
    Updated data to v0.0-2482-g0c0626eb based on 0c0626ebbf6ded92fe84dbb8cdb558383c99adf2 from https://github.com/lowRISC/ibex.
    > commit 0c0626ebbf6ded92fe84dbb8cdb558383c99adf2
    > Author: Harry Callahan <hcallahan@lowrisc.org>
    > Date:   Fri Oct 28 14:19:39 2022 +0100
    >
    >     Update google_riscv-dv to google/riscv-dv@be9c75f
    >
    >     Update code from upstream repository https://github.com/google/riscv-
    >     dv to revision be9c75fe6911504c0e6e9b89dc2a7766e367c500
    >
    >     * Reserve one extra word when pushing GPRs to kernel stack (Harry
    >       Callahan)
    >     * Store user-stack-pointer on kernel stack when pushing/popping GPRs
    >       (Harry Callahan)
    >
    >     Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,33 +4,40 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2621"
-version_tuple = (0, 0, 2621)
+version_str = "0.0.post2624"
+version_tuple = (0, 0, 2624)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2621")
+    pversion = V("0.0.post2624")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2479"
-data_version_tuple = (0, 0, 2479)
+data_version_str = "0.0.post2482"
+data_version_tuple = (0, 0, 2482)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2479")
+    pdata_version = V("0.0.post2482")
 except ImportError:
     pass
-data_git_hash = "ed927be38770b3478f34d4eace6edc1d74decaa8"
-data_git_describe = "v0.0-2479-ged927be3"
+data_git_hash = "0c0626ebbf6ded92fe84dbb8cdb558383c99adf2"
+data_git_describe = "v0.0-2482-g0c0626eb"
 data_git_msg = """\
-commit ed927be38770b3478f34d4eace6edc1d74decaa8
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Thu Oct 27 15:56:25 2022 +0100
+commit 0c0626ebbf6ded92fe84dbb8cdb558383c99adf2
+Author: Harry Callahan <hcallahan@lowrisc.org>
+Date:   Fri Oct 28 14:19:39 2022 +0100
 
-    [cov] Remove ignored_csrs coverpoints
+    Update google_riscv-dv to google/riscv-dv@be9c75f
     
-    These related to unimplemented CSRs. These are already captured by one
-    of the illegal instruction categories.
+    Update code from upstream repository https://github.com/google/riscv-
+    dv to revision be9c75fe6911504c0e6e9b89dc2a7766e367c500
+    
+    * Reserve one extra word when pushing GPRs to kernel stack (Harry
+      Callahan)
+    * Store user-stack-pointer on kernel stack when pushing/popping GPRs
+      (Harry Callahan)
+    
+    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>
 
 """
 
