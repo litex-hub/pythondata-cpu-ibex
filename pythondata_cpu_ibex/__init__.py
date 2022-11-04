9968985b1f33fd9304fa87c639f739fc62f5a1eb
LiteX Robot <robot@timvideos.us>
2022-11-04 16:17:21 +0000

    Updating pythondata-cpu-ibex to 0.0.post2664
    Updated data to v0.0-2522-g7b0921e1 based on 7b0921e14cca9bcba02e9492b22f9a185dbfdc7a from https://github.com/lowRISC/ibex.
    > commit 7b0921e14cca9bcba02e9492b22f9a185dbfdc7a
    > Author: Andreas Kurth <adk@lowrisc.org>
    > Date:   Fri Oct 28 10:09:02 2022 +0000
    >
    >     [dv] Add test glitching icache data or tag
    >
    >     This commit addresses the integrity part of #1759 by verifying that a
    >     glitch on the data or tag response from the instruction cache raises a
    >     minor alert by the core (if the data/tag returned from the cache is used
    >     by the core).
    >
    >     Signed-off-by: Andreas Kurth <adk@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2661"
-version_tuple = (0, 0, 2661)
+version_str = "0.0.post2664"
+version_tuple = (0, 0, 2664)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2661")
+    pversion = V("0.0.post2664")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2519"
-data_version_tuple = (0, 0, 2519)
+data_version_str = "0.0.post2522"
+data_version_tuple = (0, 0, 2522)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2519")
+    pdata_version = V("0.0.post2522")
 except ImportError:
     pass
-data_git_hash = "66b3ed1ac1df79cc6d285e586135fd1e68ab198f"
-data_git_describe = "v0.0-2519-g66b3ed1a"
+data_git_hash = "7b0921e14cca9bcba02e9492b22f9a185dbfdc7a"
+data_git_describe = "v0.0-2522-g7b0921e1"
 data_git_msg = """\
-commit 66b3ed1ac1df79cc6d285e586135fd1e68ab198f
+commit 7b0921e14cca9bcba02e9492b22f9a185dbfdc7a
 Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Fri Oct 28 20:24:31 2022 +0000
+Date:   Fri Oct 28 10:09:02 2022 +0000
 
-    [dv] Add test glitching register file read data
+    [dv] Add test glitching icache data or tag
     
-    This commit addresses the integrity checking part of #1756 by verifying
-    that a glitch on the data read from the register file raises a major
-    alert by the core (if the data read from the register file is used by
-    the core).
+    This commit addresses the integrity part of #1759 by verifying that a
+    glitch on the data or tag response from the instruction cache raises a
+    minor alert by the core (if the data/tag returned from the cache is used
+    by the core).
     
     Signed-off-by: Andreas Kurth <adk@lowrisc.org>
 
