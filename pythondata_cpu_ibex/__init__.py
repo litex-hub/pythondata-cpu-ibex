98b07e7283b6b65f6a2a6ee72a9fff031113c9b5
LiteX Robot <robot@timvideos.us>
2022-11-04 13:38:32 +0000

    Updating pythondata-cpu-ibex to 0.0.post2655
    Updated data to v0.0-2513-g455dbe30 based on 455dbe30f1791ac08ba2a2e9e36f96e0ee294678 from https://github.com/lowRISC/ibex.
    > commit 455dbe30f1791ac08ba2a2e9e36f96e0ee294678
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed Nov 2 10:29:28 2022 +0000
    >
    >     [dv] Add missing isolation forks
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,39 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2654"
-version_tuple = (0, 0, 2654)
+version_str = "0.0.post2655"
+version_tuple = (0, 0, 2655)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2654")
+    pversion = V("0.0.post2655")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2512"
-data_version_tuple = (0, 0, 2512)
+data_version_str = "0.0.post2513"
+data_version_tuple = (0, 0, 2513)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2512")
+    pdata_version = V("0.0.post2513")
 except ImportError:
     pass
-data_git_hash = "120607f4f2d96c6f7cdade85144463ffc3864d84"
-data_git_describe = "v0.0-2512-g120607f4"
+data_git_hash = "455dbe30f1791ac08ba2a2e9e36f96e0ee294678"
+data_git_describe = "v0.0-2513-g455dbe30"
 data_git_msg = """\
-commit 120607f4f2d96c6f7cdade85144463ffc3864d84
-Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Wed Nov 2 17:11:47 2022 +0000
+commit 455dbe30f1791ac08ba2a2e9e36f96e0ee294678
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Wed Nov 2 10:29:28 2022 +0000
 
-    [xlm] Enable access to signals without dumping waves
-    
-    The `-access rw` option allows Xcelium to access signals in the design
-    (e.g., with `uvm_hdl_read` or `uvm_hdl_force`) without having to dump
-    waves (which substantially increases the run time).  See [1] for the
-    background discussion.
-    
-    [1]: https://github.com/lowRISC/ibex/pull/1879#issuecomment-1300216022
-    
-    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
+    [dv] Add missing isolation forks
 
 """
 
