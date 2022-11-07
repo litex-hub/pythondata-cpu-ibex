a58b0f533636cf3051bd492caa7732d1ed3c5ce9
LiteX Robot <robot@timvideos.us>
2022-11-07 17:23:25 +0000

    Updating pythondata-cpu-ibex to 0.0.post2676
    Updated data to v0.0-2534-g7592bb94 based on 7592bb9478653a04a59c1ab14c009b14be9f72e9 from https://github.com/lowRISC/ibex.
    > commit 7592bb9478653a04a59c1ab14c009b14be9f72e9
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed Nov 2 16:35:18 2022 +0000
    >
    >     [dv] Disable bad integrity on uninit accesses for mem error tests
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_ibex/__init__.py
+++ pythondata_cpu_ibex/__init__.py
@@ -4,32 +4,30 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/lowRISC/ibex"
 
 # Module version
-version_str = "0.0.post2665"
-version_tuple = (0, 0, 2665)
+version_str = "0.0.post2676"
+version_tuple = (0, 0, 2676)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post2665")
+    pversion = V("0.0.post2676")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post2523"
-data_version_tuple = (0, 0, 2523)
+data_version_str = "0.0.post2534"
+data_version_tuple = (0, 0, 2534)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post2523")
+    pdata_version = V("0.0.post2534")
 except ImportError:
     pass
-data_git_hash = "bd8bee8a729cdc6e26d81061ef3c1f2eba49513f"
-data_git_describe = "v0.0-2523-gbd8bee8a"
+data_git_hash = "7592bb9478653a04a59c1ab14c009b14be9f72e9"
+data_git_describe = "v0.0-2534-g7592bb94"
 data_git_msg = """\
-commit bd8bee8a729cdc6e26d81061ef3c1f2eba49513f
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Fri Nov 4 17:40:25 2022 +0000
+commit 7592bb9478653a04a59c1ab14c009b14be9f72e9
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Wed Nov 2 16:35:18 2022 +0000
 
-    [dv] Don't set sync_exc_seen in Debug Mode
-    
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    [dv] Disable bad integrity on uninit accesses for mem error tests
 
 """
 
